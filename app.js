e6bb730744d0f0fecd37a505e69c019211ee0801
Anton Gilgur <agilgur5@gmail.com>
2018-05-03 00:06:20 -0700

    (feat): add a title + header

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const companyName = url.searchParams.get('company')
 const companyNameEnc = companyName.split(' ').join('+')
 const baseGoogleURL = 'https://www.google.com/search?q=' + companyNameEnc + '+'
 
+// add title + header
+document.title = companyName
+const header = document.createElement('h4')
+header.textContent = companyName
+header.style.textTransform = 'capitalize'
+header.style.margin = '0'
+document.body.appendChild(header)
+
+// add hyperlinks
 const sites = ['glassdoor', 'angellist', 'linkedin', 'crunchbase']
 sites.forEach((site) => {
   const anchor = document.createElement('a')
