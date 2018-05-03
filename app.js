dd84ce4e37129b1c2f90fbf8990c9bcba863fa0b
Anton Gilgur <agilgur5@gmail.com>
2018-05-02 23:23:31 -0700

    (feat): add simple goog search for each site based on query

---
+++ app.js
@@ -0,0 +1,16 @@
+const url = new URL(window.location.href)
+const companyName = url.searchParams.get('company')
+
+const iframe = document.createElement('iframe')
+iframe.style.overflow = 'auto'
+iframe.style.height = window.height // each iframe should fill the window
+
+// put it into google because that tends to be more accurate than direct search
+const companyNameEnc = companyName.split(' ').join('+')
+const baseGoogleURL = 'https://www.google.com/search?q=' + companyNameEnc + '+'
+
+['glassdoor', 'angellist', 'linkedin', 'crunchbase'].forEach((site) => {
+  const newFrame = iframe.cloneNode()
+  newFrame.setAttribute('src', baseGoogleURL + site)
+  document.body.appendChild(newFrame)
+})
