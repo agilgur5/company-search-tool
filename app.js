0263a8e3b59f65f11833d73445795158f69fcca8
Anton Gilgur <agilgur5@gmail.com>
2018-05-02 23:58:14 -0700

    (fix): use anchor tags instead of iframe bc x-frame-options
    - Google has it set to SAMEORIGIN and probably has a strict CSP as
      well, so iframes didn't work as easily as I thought
      - woops didn't even think about security ramifications of using
        iframes
    
    - use anchor tags instead of just window.open'ing a bunch because
      less jarring and because visited styling is nice / quite useful

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const url = new URL(window.location.href)
 const companyName = url.searchParams.get('company')
 
-const iframe = document.createElement('iframe')
-iframe.style.overflow = 'auto'
-iframe.style.height = window.height // each iframe should fill the window
-
 // put it into google because that tends to be more accurate than direct search
 const companyNameEnc = companyName.split(' ').join('+')
 const baseGoogleURL = 'https://www.google.com/search?q=' + companyNameEnc + '+'
 
 const sites = ['glassdoor', 'angellist', 'linkedin', 'crunchbase']
 sites.forEach((site) => {
-  const newFrame = iframe.cloneNode()
-  newFrame.setAttribute('src', baseGoogleURL + site)
-  document.body.appendChild(newFrame)
+  const anchor = document.createElement('a')
+  anchor.setAttribute('href', baseGoogleURL + site)
+  anchor.textContent = site
+  document.body.appendChild(anchor)
+  document.body.appendChild(document.createElement('br'))
 })
